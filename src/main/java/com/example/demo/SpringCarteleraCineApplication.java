907c080e4e8da34dfa397334a5a50e2f56515596
Leticia Orive Peña <letimedi17031994@gmail.com>
2021-02-22 15:37:48 +0100

    subir el springCarteleraCine

--- src/main/java/com/example/demo/SpringCarteleraCineApplication.java
+++ src/main/java/com/example/demo/SpringCarteleraCineApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.repository.CinemaRepository;
+
 @SpringBootApplication
 public class SpringCarteleraCineApplication {
+	@Autowired
+	private CinemaRepository cinemaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCarteleraCineApplication.class, args);
 	}
+	public void run(String... args) throws Exception {
 
+		
+		// Crear datos
+	}
 }
